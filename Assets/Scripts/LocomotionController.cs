15f79e2328527abef9fa6455246a642a3dc7eb09
Dovydas <BQ@DESKTOP-A6RJ1P4>
2021-01-03 07:16:32 -0800

    Initial setup, some functionality

---
+++ Assets/Scripts/LocomotionController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class LocomotionController : MonoBehaviour
+{
+
+    public XRController leftTeleportRay;
+
+    public InputHelpers.Button teleportActivationButton;
+   
+    void Update()
+    {
+        if (leftTeleportRay)
+        {
+            leftTeleportRay.gameObject.SetActive(CheckIfActivated(leftTeleportRay));
+        }
+    }
+
+    public bool CheckIfActivated(XRController controller)
+    {
+        InputHelpers.IsPressed(controller.inputDevice, teleportActivationButton, out bool isActivated);
+        return isActivated;
+    }
+}
---
+++ Assets/Scripts/Pistol.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pistol : MonoBehaviour
+{
+
+    public float speed = 40f;
+    public GameObject bullet;
+    public Transform barrel;
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    public void Fire()
+    {
+        //GameObject spawnedBullet = Instantiate(bullet, barrel.position, barrel.rotation);
+       // spawnedBullet.GetComponent<Rigidbody>().velocity = speed * barrel.forward;
+        audioSource.PlayOneShot(audioClip);
+        animator.SetTrigger("Shoot");
+        //Destroy(spawnedBullet, 2f);
+    }
+
+}
