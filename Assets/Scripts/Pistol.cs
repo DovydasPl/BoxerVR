92abe60c25927c6d420d3d33708acad266951c9d
Dovydas <BQ@DESKTOP-A6RJ1P4>
2021-01-04 12:22:41 -0800

    Added some gun fire logic, cartridge flies out of gun

--- Assets/Scripts/Pistol.cs
+++ Assets/Scripts/Pistol.cs
@@ -5,25 +5,52 @@ using UnityEngine;
 public class Pistol : MonoBehaviour
 {
 
-    public float speed = 40f;
-    public GameObject bullet;
-    public Transform barrel;
+    public float range = 40f;
+    public Transform gunPoint;
+    public GameObject bulletCartridge;
+    public Transform bulletEjectPoint;
     public AudioSource audioSource;
     public AudioClip audioClip;
+    public LayerMask gunHitLayer;
 
+
+    private int numberOfAmmo = 10;
     private Animator animator;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
     }
-    public void Fire()
+
+    private void Update()
     {
-        //GameObject spawnedBullet = Instantiate(bullet, barrel.position, barrel.rotation);
-       // spawnedBullet.GetComponent<Rigidbody>().velocity = speed * barrel.forward;
-        audioSource.PlayOneShot(audioClip);
+        Debug.DrawRay(gunPoint.position, gunPoint.forward * range, Color.green);
+
+    }
+    public void PullTrigger()
+    {
+        if (numberOfAmmo <= 0) return;
         animator.SetTrigger("Shoot");
-        //Destroy(spawnedBullet, 2f);
+        numberOfAmmo--;
+
+    }
+
+    public void ShootBullet()
+    {
+        audioSource.PlayOneShot(audioClip);
+        RaycastHit hit;
+        if (Physics.Raycast(gunPoint.position, gunPoint.forward, out hit, range, gunHitLayer))
+        {
+            if (hit.collider.tag == "Ball") Destroy(hit.collider.gameObject);
+        }
+    }
+
+    public void EjectCartridge()
+    {
+        GameObject tempCartridge = Instantiate(bulletCartridge, bulletEjectPoint.position, bulletEjectPoint.rotation) as GameObject;
+        tempCartridge.GetComponent<Rigidbody>().AddForce(bulletEjectPoint.right * -1 * Random.Range(100f, 150f) + bulletEjectPoint.forward * -1 * Random.Range(100f, 150f));
+        tempCartridge.GetComponent<Rigidbody>().AddTorque(new Vector3(0, Random.Range(100f, 500f), Random.Range(100f, 1000f)), ForceMode.Impulse);
+        Destroy(tempCartridge, 5f);
     }
 
 }
